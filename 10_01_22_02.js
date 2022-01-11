9a6172343bb888e9ab02ae65937b301d09a52a3b
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-11 08:44:29 +0700

    new pj

---
+++ 10_01_22_02.js
@@ -0,0 +1,9 @@
+tem = Number(prompt("Nhập nhiệt độ hiện tại"));
+
+if(tem>100){
+    alert("Xin hãy giảm nhiệt độ");
+}else if (tem<20){
+    alert("Xin hãy tăng nhiệt độ");
+}else{
+    alert("giữ thế này nhé (:");
+}
